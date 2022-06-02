e0ff7e2782f286046106e32c7789782b70df91c3
Christine Bui <Christine.Bui@student.csulb.edu>
2022-06-01 22:48:41 -0700

    added incomplete functions

---
+++ lab1.py
@@ -0,0 +1,19 @@
+'''
+Name: Christine Bui
+Partner: Matthew Eskridge
+Programming Assignment 1
+'''
+
+def bubbleSort():
+    pass
+
+def insertionSort():
+    pass
+
+def quickSort():
+    pass
+
+# EXTRA CREDIT
+def mergeSort():
+    pass
+
