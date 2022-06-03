bb83746285a0c44caebcd7b27bc277f49ab14dc8
Matthew Eskridge <mattheweskridge@gmail.com>
2022-06-02 20:36:19 -0700

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -4,8 +4,15 @@ Partner: Matthew Eskridge
 Programming Assignment 1
 '''
 
-def bubbleSort():
-    pass
+def bubbleSort(data):
+    #pass
+    for i in (1, len(data) - 1):
+        for j in (len(data), i + 1, -1):
+            if data[j] < data[j - 1]:
+                placeholder = data[j]
+                data[j] = data[j - 1]
+                data[j - 1] = placeholder
+    return data
 
 def insertionSort():
     pass
@@ -17,3 +24,5 @@ def quickSort():
 def mergeSort():
     pass
 
+data = [4, 3, 6, 2, 7, 4, 12, 8, 9]
+print(bubbleSort(data))
